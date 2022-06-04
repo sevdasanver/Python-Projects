2ab1c3226629c188d94e9674385474803f3aa020
Sevda Sanver <sevdasanver@hotmail.com>
2022-06-04 01:08:02 -0400

    Computer guess game

--- 2-ComputerGuessTheNumber/main.py
+++ 2-ComputerGuessTheNumber/main.py
@@ -3,6 +3,7 @@ import random
 def computer_guess():
     start, end = 1, 100
     back = ''
+
     while back != "c":
         if start <= end:
             guess = random.randint(start, end)
@@ -11,6 +12,7 @@ def computer_guess():
 
         back = input(f"If {guess} is greater than the value you have on mind enter h,\n if lower enter l,\n if correct enter c.\n If you want to exit the game, press any key.").lower()
         print("---*---")
+        
         if back == 'h':
             end = guess - 1
         elif back == 'l':
